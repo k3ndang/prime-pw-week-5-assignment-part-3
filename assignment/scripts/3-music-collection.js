1c2ac49a2e6e9e520abeb17c47afac3ee1495a4d
Ken Dang <k3ndang@gmail.com>
2021-11-26 22:05:41 -0600

    working on the assignment

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,3 +2,20 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 console.log(collection);
+
+// Adding music CD to collection
+function addToCollection(albumTitle, albumArtist, yearPublished) {
+// the variable album contains the following
+  let album = {
+    title:  albumTitle,
+    artist: albumArtist,
+    year: yearPublished,
+  };
+// adding the album to variable collection
+  collection.push(album);
+  return album;
+}
+
+// testing the function
+console.log(addToCollection('Que Huong Toi', 'Manh Quynh', '1996'));
+console.log(collection);
